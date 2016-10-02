2f91071282ca7077415942fd61ee0cf695af8d70
dedalus <sheff57@gmail.com>
2016-10-01 20:52:02 -0400

    add and update document

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
 def my_square(y):
 	"""takes a value and returns the square of that value. 
 	added more documentation
-	xxxxxooooxxxxoooxxxooxxxooo
+	uses the ** exponent operator
 	"""
 	return(y**2)
 
